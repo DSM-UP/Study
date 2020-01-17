945069a62eae1adc878497b768364fedb1759133
BuildTools <unconfigured@null.spigotmc.org>
2020-01-17 23:49:08 +0900

    2020/01/17 mfc browser

---
+++ 손정우/2020-01/2020_01_17/framework.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // 일부 CString 생성자는 명시적으로 선언됩니다.
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // 대화 상자에서 MFC 컨트롤에 대한 지원을 제거합니다.
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#endif
+
+#include <afx.h>
+#include <afxwin.h>         // MFC 핵심 및 표준 구성 요소입니다.
+#include <afxext.h>         // MFC 확장입니다.
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // Internet Explorer 4 공용 컨트롤에 대한 MFC 지원입니다.
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>                     // Windows 공용 컨트롤에 대한 MFC 지원입니다.
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <iostream>
