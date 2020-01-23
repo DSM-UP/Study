9b60b74932ccb0db322eab13caa5eefbc568e8bd
BuildTools <unconfigured@null.spigotmc.org>
2020-01-23 23:26:13 +0900

    2020/01/23 TIL

---
+++ 손정우/2020-01/2020_01_23/CHistoryDlg.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+
+// CHistoryDlg 대화 상자
+
+class CHistoryDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CHistoryDlg)
+
+public:
+	CHistoryDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CHistoryDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_HISTORY };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+private:
+//	CListBox m_listURL;
+public:
+//	afx_msg void OnSelchangeListUrl();
+	CString m_strSeleted;
+	CListCtrl m_listURL;
+};
---
+++ 손정우/2020-01/2020_01_23/MyBrowserView.h
@@ -0,0 +1,50 @@
+﻿
+// MyBrowserView.h: CMyBrowserView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CMyBrowserView : public CHtmlView
+{
+protected: // serialization에서만 만들어집니다.
+	CMyBrowserView() noexcept;
+	DECLARE_DYNCREATE(CMyBrowserView)
+
+// 특성입니다.
+public:
+	CMyBrowserDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+
+// 구현입니다.
+public:
+	virtual ~CMyBrowserView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnMypageNaver();
+	afx_msg void OnMypageGoogle();
+	afx_msg void OnMypageUserinput();
+};
+
+#ifndef _DEBUG  // MyBrowserView.cpp의 디버그 버전
+inline CMyBrowserDoc* CMyBrowserView::GetDocument() const
+   { return reinterpret_cast<CMyBrowserDoc*>(m_pDocument); }
+#endif
+
