52e0a12484d974c6cfe49b92139def30e7c58d50
Sonwanseo <sonwanseo@gmail.com>
2020-01-30 13:45:58 +0900

    2020/01/30/TIL

---
+++ 손완서/2020_1월/2020_01_30/src/16.1.4.js
@@ -0,0 +1,6 @@
+const x = 12;
+x.toString(); // "12"
+x.toString(10); // "12"
+x.toString(16); // "c"
+x.toString(8); // "14"
+x.toString(2); // "1100"
---
+++ 손완서/2020_1월/2020_01_30/src/16.4.js
@@ -0,0 +1,6 @@
+function deg2rad(d) {
+  return (d / 180) * Math.PI;
+}
+function rad2deg(r) {
+  return (r / Math.PI) * 180;
+}
