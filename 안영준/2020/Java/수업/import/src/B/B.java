0adaf6ba4de0f02a0146a6b74ab99b301f83030a
Ahnyeongjun <toinin@naver.com>
2020-06-09 18:04:04 +0900

    2020/06/09

---
+++ 안영준/2020/Java/수업/import/src/B/B.java
@@ -0,0 +1,5 @@
+package B;
+
+public class B {
+
+}
---
+++ 안영준/2020/Java/수업/import/src/a/a.java
@@ -0,0 +1,15 @@
+package a;
+
+import B.B;
+
+public class a {
+	public static void main(String[] args) {
+		Object o = new Object();
+		System.out.println(o.toString());
+		String s = new String("bi");
+		System.out.println(s);
+		B b = new B();
+		System.out.println(b);
+	}
+
+}
