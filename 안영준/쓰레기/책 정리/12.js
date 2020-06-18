59792822121405f73f141467525f37df4c211a85
Ahnyeongjun <toinin@naver.com>
2020-06-18 20:03:57 +0900

    2020/06/18

---
+++ 안영준/쓰레기/책 정리/12.js
@@ -0,0 +1,68 @@
+/*const num1 = 1;
+const num2 = 2;
+
+const result = `${num1}${num2}`
+console.log(result);*/
+/*function Person(name,age,hobby){
+    this.name = name;
+    this.age = age;
+    this.hobby = htbby;
+}
+
+var foo = new Person('foo',36,'game');
+console.log(foo.hasOwnProperty('name'));
+console.dir(Person.prototype);
+*/
+/*
+
+function Person(name){
+    Person.prototype.getName = function(){
+        return this.name;
+    }
+    var foo = new Person('Foo');
+  
+    console.log(foo.getName);
+    Person.prototype.name= 'Person';
+    console.log(Person.prototype.getName());
+ }*/
+ /*
+ console.log(Person.prototype.constructor);
+
+ var foo = new Person('foo');
+ console.log(foo.country);
+
+ Person prototype = {
+     country:'korea',
+ };
+ console.log(Person.prototype.constructor);
+ foo.prototype = Person.prototype;
+
+ var bar= new Person('bar');
+
+console.log(foo.country);
+console.dir(foo.country);*//*
+function execute(param1,param2){
+    var a = 1, b=2;
+    return a+b;
+}
+return param1= param2 + func();
+
+execute(1,2);
+function b(paa1,fatch)    */
+
+/*
+function execute (param1,param2){
+
+var a =1,b=2;
+function func(){
+    return a+b;
+}
+return param1+param2+func();
+}
+execute(3,4);*/
+
+var var1 = 1;
+var var2 = 2;
+
+console.log(var1);
+console.log(var2);
