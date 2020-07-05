821e482e77bd30f7c66cfc57ff1bff87940493ba
joungeun <lje0818ovo@naver.com>
2020-07-05 15:08:54 +0900

    up challenge

---
+++ 이종은/2020/숙제/up_Challenge_problem6.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int i, j, value =0;
+    for (i = 0; i < 8; i++)
+    {
+        value += 3;
+    }
+    value /= 4;
+
+    printf("%d", value);
+
+    return 0;
+}
---
+++ 이종은/2020/숙제/up_Challenge_problem8.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#define num 5
+
+int main()
+{
+    int arr[num] = { 5, 10, 15 };
+    int sum=0, i;
+    for (i = 0; i < num; i++)
+    {
+        sum += arr[i];
+    }
+    printf("%d", sum);
+
+    return 0;
+}
