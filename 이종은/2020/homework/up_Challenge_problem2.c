d4f31f363d8bd7fe023494c0f2b305e497afa339
joungeun <lje0818ovo@naver.com>
2020-07-06 19:31:16 +0900

    up 문제첼린지

---
+++ 이종은/2020/homework/up_Challenge_problem2.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num,sum=0;
+    printf("정수를 입력하세요. : ");
+    scanf("%d", &num);
+    while (num != 0)
+    {
+        sum += num;
+        printf("정수를 입력하세요. : ");
+        scanf("%d", &num);
+
+        
+    }
+    printf("%d", sum);
+    
+
+    return 0;
+}
---
+++ 이종은/2020/homework/up_Challenge_problem7.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char arr[100] = "";
+    int i;
+    gets(arr);
+    for (i = 0; arr[i]!='\0'; i++)
+    {
+        if (arr[i] >= 'A' && arr[i] <= 'Z')
+        {
+            arr[i] = arr[i] + 32;
+        }
+        else if(arr[i] >= 'a' && arr[i] <= 'z')
+        {
+             arr[i] = arr[i] - 32;
+        }
+    }
+    puts(arr);
+
+    return 0;
+}
