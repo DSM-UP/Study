8276ce468f48e60278929d6e1f66f90fe0633ab4
joungeun <lje0818ovo@naver.com>
2020-07-06 19:28:51 +0900

    20200701 구조체

---
+++ 이종은/2020/homework/20200701_up_homework_struct.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//#include <stdlib.h>
+typedef struct{
+    int num;
+    char blood[3];
+    float eyes;
+    int weight;
+
+}Privacy;
+
+int main()
+{
+    int i;
+    Privacy man[2];
+
+
+    for (i = 0; i < 2; i++)
+    {
+        scanf("%d %s %f %d", &man[i].num, &man[i].blood, &man[i].eyes, &man[i].weight); //함수로 만들기!
+    }
+    for (i = 0; i < 2; i++)
+    {
+        printf("%d %s %.1f %d", man[i].num, man[i].blood, man[i].eyes, man[i].weight);
+        putchar('\n');
+    }
+
+    return 0;
+}
