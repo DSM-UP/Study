9283add910238d723c59f5e11c508a73956b2633
Ahnyeongjun <toinin@naver.com>
2020-07-09 07:51:29 +0900

    2020/07/09

--- 안영준/2020/Java/수업/Study_CLASS/src/Stack_2020_07_07/Stack_Ex.java
+++ 안영준/2020/Java/수업/Study_CLASS/src/Stack_2020_07_07/Stack_Ex.java
@@ -1,5 +1,20 @@
 package Stack_2020_07_07;
 
-public class Stack_Ex {
+import java.util.*;
 
+public class Stack_Ex {
+	public static void main(String[] args) {
+
+	Stack<Coin> coinbox = new Stack<Coin>(); 
+	
+	coinbox.push(new Coin(199));
+	coinbox.push(new Coin(199));
+	coinbox.push(new Coin(199));
+	coinbox.push(new Coin(199));
+	
+	while(coinbox.isEmpty()) {
+		Coin coin = coinbox.pop();
+		System.out.println(coin.getValue());
+	}
+}
 }
